6460ea6db2ab3a67d676b8029672b9d05dbe7c0c
cirt9 <bartekxzxz@gmail.com>
2017-10-21 20:04:34 +0200

    I have started to design menu for chat.

--- ChatProject/Chat/mainwindow.cpp
+++ ChatProject/Chat/mainwindow.cpp
@@ -5,7 +5,12 @@ MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent)
     MainWindow::setWindowTitle(QString("Chat"));
     resize(QDesktopWidget().availableGeometry(this).size() * 0.7);
 
-    ChatWidget * chat = new ChatWidget(40, this);
+    uiContainer = new QStackedWidget();
+    setCentralWidget(uiContainer);
+
+    createMenu();
+
+    /*ChatWidget * chat = new ChatWidget(40, this);
     chat->setObjectName("ChatWidget");
     chat->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     chat->enableMoving();
@@ -14,4 +19,38 @@ MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent)
     //setCentralWidget(chat);
 
     chat->move(10, 10);
+    */
+}
+
+void MainWindow::createMenu()
+{
+    QWidget * menuContainer = new QWidget();
+    QGridLayout * menuLayout = new QGridLayout();
+
+    QSpacerItem * leftSpacer = new QSpacerItem(0, QSizePolicy::Expanding, QSizePolicy::Expanding);
+    menuLayout->addItem(leftSpacer, 0, 0);
+    QSpacerItem * rightSpacer = new QSpacerItem(0, QSizePolicy::Expanding, QSizePolicy::Expanding);
+    menuLayout->addItem(rightSpacer, 0, 2);
+
+    QLabel * title = new QLabel("Chat");
+    title->setObjectName("MainMenuTitleLabel");
+    title->setAlignment(Qt::AlignCenter);
+    menuLayout->addWidget(title, 0, 1, Qt::AlignTop);
+
+    QLabel * author = new QLabel("Created by Bartłomiej Wójtowicz");
+    author->setObjectName("MainMenuAuthorLabel");
+    author->setAlignment(Qt::AlignRight);
+    menuLayout->addWidget(author, 1, 1, Qt::AlignTop);
+
+    QPushButton * startServerButton = new QPushButton("Create new server");
+    menuLayout->addWidget(startServerButton, 2, 1);
+
+    QPushButton * joinServerButton = new QPushButton("Join existing server");
+    menuLayout->addWidget(joinServerButton, 3, 1);
+
+    QPushButton * quitButton = new QPushButton("Quit");
+    menuLayout->addWidget(quitButton, 4, 1);
+
+    menuContainer->setLayout(menuLayout);
+    uiContainer->addWidget(menuContainer);
 }
--- ChatProject/Chat/mainwindow.h
+++ ChatProject/Chat/mainwindow.h
@@ -4,14 +4,26 @@
 #include <QMainWindow>
 #include <QDesktopWidget>
 #include <chatwidget.h>
+#include <QStackedWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QStackedWidget * uiContainer;
+
+    void createMenu();
+
 public:
     MainWindow(QWidget * parent = 0);
     ~MainWindow() {}
+
+public slots:
+    //void displayMenu();
 };
 
 #endif // MAINWINDOW_H
