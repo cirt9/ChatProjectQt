85c8b6f62da22e519012c521786d60772181c79b
cirt9 <bartekxzxz@gmail.com>
2017-12-18 20:06:56 +0100

    Styling

--- ChatProject/Chat/sidemenu.cpp
+++ ChatProject/Chat/sidemenu.cpp
@@ -18,12 +18,14 @@ SideMenu::SideMenu(QWidget * parent) : QWidget(parent)
 void SideMenu::initializeButtonsContainer()
 {
     buttonsContainer = new QWidget();
+    buttonsContainer->setObjectName("SideMenuButtonsContainer");
 
     buttonsLayout = new QVBoxLayout();
-    buttonsLayout->setSpacing(0);
+    buttonsLayout->setSpacing(10);
     buttonsLayout->setAlignment(Qt::AlignTop);
-    buttonsLayout->setContentsMargins(0, 0, 0, 0);
+    buttonsLayout->setContentsMargins(4, 10, 4, 10);
     buttonsContainer->setLayout(buttonsLayout);
+    buttonsContainer->hide();
     layout->addWidget(buttonsContainer);
 }
 
@@ -32,8 +34,8 @@ void SideMenu::initializeTabs()
     tabs = new QStackedWidget();
     tabs->setObjectName("SideMenuContainer");
     tabs->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
-    tabs->setFixedWidth(100);
-    tabs->setStyleSheet("background: white");
+    tabs->setFixedWidth(350);
+    tabs->hide();
     layout->addWidget(tabs);
 }
 
@@ -50,11 +52,14 @@ void SideMenu::createSideButton()
 void SideMenu::addNewTab(QWidget * widget)
 {
     QToolButton * button = new QToolButton();
+    button->setFixedSize(60, 60);
+    button->setObjectName("SideMenuToolButton");
     button->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
     button->setProperty("id", getAvailableId());
     connect(button, SIGNAL(clicked(bool)), this, SLOT(changeTab()));
 
     buttonsLayout->addWidget(button);
+    buttonsLayout->setAlignment(button, Qt::AlignTop);
     buttons.append(button);
 
     tabs->addWidget(widget);
